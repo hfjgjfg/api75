62f7173aec39e08151af83e11921346b3c6584b1
hfjgjfg <mohammadhaselmehri20202020@gmail.com>
2016-03-03 12:03:34 +0330

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -5,7 +5,7 @@ URL = require('socket.url')
 JSON = require('dkjson')
 HTTPS = require('ssl.https')
 ----config----
-local bot_api_key = ""
+local bot_api_key = "203893006:AAHUGqk5TWTuPSlou8ZwaQPNGsT02nVmMsM"
 local BASE_URL = "https://api.telegram.org/bot"..bot_api_key
 local BASE_FOLDER = ""
 local start = [[HI
@@ -55,7 +55,7 @@ _just send a photo_
 
 function is_admin(msg)-- Check if user is admin or not
   local var = false
-  local admins = {}-- put your id here
+  local admins = {150575718}-- put your id here
   for k,v in pairs(admins) do
     if msg.from.id == v then
       var = true
